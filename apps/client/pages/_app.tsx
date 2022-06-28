b003bd12720fad56b3235c27ad4f5892b8fe4a59
Maxime PIGEON <maxpige@gmail.com>
2022-06-28 19:58:02 +0200

    first connection between front and back with tipio1

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -1,16 +1,24 @@
 import { AppProps } from 'next/app';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import Head from 'next/head';
 import './styles.css';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3333/graphql',
+  cache: new InMemoryCache(),
+});
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <title>Welcome to jeuxvoisins-client!</title>
-      </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
+      <ApolloProvider client={client}>
+        <Head>
+          <title>Welcome to jeuxvoisins-client!</title>
+        </Head>
+        <main className="app">
+          <Component {...pageProps} />
+        </main>
+      </ApolloProvider>
     </>
   );
 }
